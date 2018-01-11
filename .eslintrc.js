488173cc42cc8ed8a119d2e1f28099565e069d14
Jace Babin <jbabin91@gmail.com>
2018-01-11 01:02:19 -0700

    Started working with react-router

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   extends: ['airbnb'],
   rules: {
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to']
+      }
+    ],
     'react/jsx-filename-extension': [
       1,
       {
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
 
 import '../node_modules/normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
